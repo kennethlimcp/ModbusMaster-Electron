b50d8441dc71c35d236ca762b03d3f03bc126e92
Kenneth Lim <kennethlimcp@gmail.com>
2018-06-21 22:43:37 +0800

    wip
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
+PARTICLE_PLATFORM=photon
+VERSION=0.6.3
+
 all: ${PARTICLE_PLATFORM}.bin
 
 ${PARTICLE_PLATFORM}.bin: examples/usage/usage.ino src/ModbusMaster-Particle.*
-	particle compile ${PARTICLE_PLATFORM} $< --saveTo ${PARTICLE_PLATFORM}.bin
+	particle compile ${PARTICLE_PLATFORM} $< --saveTo ${PARTICLE_PLATFORM}.bin --target ${VERSION}
--- examples/usage/usage-backup.ino
+++ examples/usage/usage-backup.ino
@@ -3,9 +3,6 @@ SYSTEM_MODE(MANUAL);
 
 #include "ModbusMaster-Particle.h"
 
-#define LED_DEBUG D7
-
-//42482d3d
 SerialLogHandler logHandler(9600,LOG_LEVEL_WARN, {
     {"app", LOG_LEVEL_TRACE},
     {"system", LOG_LEVEL_INFO}
@@ -27,28 +24,15 @@ void idle() {
 }
 
 void setup() {
-    pinMode(LED_DEBUG, OUTPUT);
-    digitalWrite(LED_DEBUG, LOW);
     slave.begin(1,Serial1); // slaveID=1, serial=Serial1
-    slave.setSpeed(19200,SERIAL_8E1);
+    slave.setSpeed(9600,SERIAL_8N1);
     // slave.setSpeed(9600); // same as above
     slave.enableDebug(); // to catch the logs
     slave.preTransmission(preTransmission);
     slave.postTransmission(postTransmission);
     slave.idle(idle);
-/*
-    uint8_t result = slave.readHoldingRegisters(3918,2);
 
-    if (!result) {
-        uint16_t value = slave.getResponseBuffer(0);
-        Log.info("Received: %0x",value);
-    } else {
-        Log.warn("Read error");
-    } */
-}
-
-void loop() {
-    uint8_t result = slave.readHoldingRegisters(3914,4);
+    uint8_t result = slave.readHoldingRegisters(8000,1);
 
     if (!result) {
         uint16_t value = slave.getResponseBuffer(0);
@@ -56,7 +40,8 @@ void loop() {
     } else {
         Log.warn("Read error");
     }
+}
 
-    delay(5000);
-    digitalWrite(LED_DEBUG, !digitalRead(LED_DEBUG));
+void loop() {
+    // do nothing :P
 }
